ba3d412289f7d87a58f82e2dfb4b1fe73b19d317
Etienne Le Montreer <etienne.lemontreer@gmail.com>
2015-05-26 00:22:34 +0200

    Website generation with Couscous

--- src/BaBoedoeh/AdminBundle/BaBoedoehAdminBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace BaBoedoeh\AdminBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class BaBoedoehAdminBundle extends Bundle
-{
-}
--- src/BaBoedoeh/BookingBundle/BaBoedoehBookingBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace BaBoedoeh\BookingBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class BaBoedoehBookingBundle extends Bundle
-{
-}
