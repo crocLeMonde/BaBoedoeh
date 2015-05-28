dbb6e7512f261022fc55be52671bcfb73506d471
Etienne Le Montreer <etienne.lemontreer@gmail.com>
2015-05-29 00:16:48 +0200

    Parameters.dist secret Session ID 404, 500 and fallback error pages customized

--- src/BaBoedoeh/BookingBundle/Controller/HomeController.php
+++ src/BaBoedoeh/BookingBundle/Controller/HomeController.php
@@ -5,11 +5,13 @@ namespace BaBoedoeh\BookingBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class HomeController extends Controller
 {
     /**
      * @Route("/")
+     * @Method("GET")
      * @Template()
      */
     public function homeAction()
